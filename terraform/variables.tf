58e8a5ba9c927f959ba7c8069dce47aa0fbc84bb
Ismael Lima <156124868+ismaellimagoncalves@users.noreply.github.com>
2025-08-18 21:59:30 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,11 @@
 variable "aws_region" {
   description = "Região AWS"
+  type        = string
   default     = ""
 }
 
 variable "ec2_key_name" {
-  description = "chave de acesso para a instância EC2"
-  type        = string
-}
-
-variable "dockerhub_user" {
-  description = "Usuário do Docker Hub"
-  type        = string
-}
-
-variable "dockerhub_image" {
-  description = "Nome da imagem Docker Hub"
-  type        = string
-}
-variable "dockerhub_password" {
-  description = "Senha do Docker Hub (opcional, se usar login automático)"
+  description = "Chave de acesso para a instância EC2"
   type        = string
   default     = ""
 }
